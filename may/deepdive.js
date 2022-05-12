a58aaa803fc4102c051b784f43b7489c82a44955
heejin <khj16355@naver.com>
2022-05-13 00:57:39 +0900

    예제

---
+++ may/deepdive.js
@@ -0,0 +1,13 @@
+var binary = 0b01000001;
+var octal = 0o101;
+var hex = 0x41;
+var hello = 0b01000001;
+var hello2 = 0b11000001;
+
+
+console.log(binary);
+console.log(octal);
+console.log(hex);
+console.log(hello2);
+console.log(binary === octal);
+console.log(octal === hex);
